f5507ac42ba417517baf63eb1d0183c69aebaad9
JEEVANANTHAM2021 <jeevanathamjeeva64@gmail.com>
2025-09-20 20:05:33 +0530

    Updated File

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <ShopContextProvider>
     <App />
-  </BrowserRouter>,
+  </ShopContextProvider>
+  </BrowserRouter>
 )
