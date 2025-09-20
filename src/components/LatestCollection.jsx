82a2387e293c64c17874f506652fc364e6288dbc
JEEVANANTHAM2021 <jeevanathamjeeva64@gmail.com>
2025-09-20 22:29:58 +0530

    Updated file

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 import ProductItem from './ProductItem';
@@ -10,20 +10,20 @@ const LatestCollection = () => {
 
     useEffect(()=>{
         setLatestProducts(products.slice(0,10));
-    },[products])
+    },[]);
 
   return (
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-            <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+        <div className='text-center py-8 text-3xl bg-gradient-to-r from-white via-gray-500 to-white'>
+            <Title text1={'TRENDY'} text2={'COLLECTIONS'} />
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni minus reprehenderit sint minima!</p>
         </div>
         {/* Rendering Products */}
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 bg-gradient-to-r from-white via-gray-500 to-white '>
             {
                 latestProducts.map((item,index)=>(
-                    <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price}/>
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
                 ))
             }
         </div>
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,12 +2,12 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link  } from 'react-router-dom';
 
-const ProductItem = (id,image,name,price) => {
+const ProductItem = ({id,image,name,price}) => {
     const {currency} = useContext(ShopContext);
   return (
-    <Link className='text-gray-300 cursor-pointer' to={`/product/${id}`}>
+    <Link className='text-gray-800 cursor-pointer' to={`/product/${id}`}>
         <div className='overflow-hidden'>
-            <img className='hover:scale-110 transistion ease-in-out' src={image[0]} alt='' />
+            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt='' />
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency}{price}</p>
@@ -15,4 +15,4 @@ const ProductItem = (id,image,name,price) => {
   )
 }
 
-export default ProductItem
+export default ProductItem;
