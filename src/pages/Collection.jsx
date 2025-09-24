ae735663a39bdb47808032398936fb9fd6cc2800
JEEVANANTHAM2021 <jeevanathamjeeva64@gmail.com>
2025-09-24 20:40:15 +0530

    Updated file

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relavent');
 
 
   const toggleCategory = (e) => {
@@ -32,13 +33,46 @@ const Collection = () => {
      }
   }
 
- useEffect(()=>{
-  console.log(category);
- },[category])
+  const applyFilter = () => {
+    
+    let productsCopy = products.slice();
+    if ( category.length > 0){
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
+    }
+    if ( subCategory.length > 0){
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory));
+    }
 
- useEffect(()=>{
-  console.log(subCategory);
- },[subCategory])
+    setFilterProducts(productsCopy);
+
+  }
+
+  const sortProducts = () => {
+
+    let fpCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a,b) => (a.price - b.price)));
+        break;
+
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b) => (b.price - a.price)));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+  }
+
+  useEffect(() =>{
+    sortProducts();
+  },[sortType])
+
+  useEffect(() => {
+    applyFilter()
+  },[category, subCategory])
 
   useEffect(()=>{
     setFilterProducts(products)
@@ -53,7 +87,7 @@ const Collection = () => {
         </p>
         
       {/* Category Filter */}
-      <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '':'hidden'} sm:block`}>
+      <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '':'hidden'} sm:block bg-gradient-to-r from-white via-gray-400 to-white`}>
         <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
         <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
           <p className='flex gap-2'>
@@ -68,7 +102,7 @@ const Collection = () => {
         </div>
       </div>
       {/* Subcategory Filter */}
-      <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '':'hidden'} sm:block`}>
+      <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '':'hidden'} sm:block bg-gradient-to-r from-white via-gray-400 to-white`}>
         <p className='mb-3 text-sm font-medium'>TYPE</p>
         <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
           <p className='flex gap-2'>
@@ -87,17 +121,17 @@ const Collection = () => {
       {/* Right side */}
       <div className='flex-1'>
 
-        <div className='flex justify-between text-base sm:text-2xl mb-5'>
+        <div onChange={(e) => setSortType(e.target.value)} className='flex justify-between text-base sm:text-2xl mb-5'>
           <Title text1= {'ALL'} text2={'COLLECTIONS'} />
           {/* Product Sort */}
-          <select className='border border-gray text:sm px-2'>
+          <select className='border border-gray text:sm px-2 bg-gradient-to-r from-white via-gray-400 to-white'>
             <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low-High</option>
             <option value="high-low">Sort by: High-Low</option>
           </select>
         </div>
         {/* Map Products */}
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6 bg-gradient-to-r from-white via-gray-500 to-white'>
           {
             filterProducts.map((item,index)=>(
               <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
