ce28ba98de1b0bcb4808ab5f6ff3f2577af2ca96
JEEVANANTHAM2021 <jeevanathamjeeva64@gmail.com>
2025-11-13 20:05:56 +0530

    Updated hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row gap-2 mt-6'>
+    <div className='flex flex-col sm:flex-row gap-2'>
       {/* Hero Left Side */}
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
         <div className='text-[#543622] bg-[#ECE7E2] text-center bg-no-repeat shadow-lg w-200 rounded-lg px-6'>
@@ -19,7 +19,7 @@ const Hero = () => {
         </div>
       </div>
       {/* Hero Right Side */}
-      <div className='relative w-full max-w-xs mx-12'>
+      <div className='relative w-full max-w-xs mx-auto'>
         <div className='border border-gray-200 bg-[#ECE7E2] rounded-2xl shadow-lg h-100'>
           <img className='absolute -top-0 left-1/2 -translate-x-1/2
            md:w-md h-full object-cover rounded-xl shadow-lg hover:scale-105 md:hover:scale-60 transition duration-300 ease-in-out'
