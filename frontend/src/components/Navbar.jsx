0dc9db81c23ee414daf1aa81b9270bbb5ccf3b17
JEEVANANTHAM2021 <jeevanathamjeeva64@gmail.com>
2025-12-10 15:13:15 +0530

    Updated Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { ShopContext } from '../context/ShopContext';
 const Navbar = () => {
 
     const [visible, setVisible] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const {setShowSearch, getCartCount, navigate, token, setToken, setCartItems} = useContext(ShopContext);
 
@@ -43,17 +44,29 @@ const Navbar = () => {
         <div className='flex items-center gap-6 pr-1 md:pr-2'>
              <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
 
-             <div className='group relative'>
-                <img onClick={() => token ? null : navigate('/login')} src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
-                {/* DropDown Menu */}
-                { token && <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-gray-200 text-gray-500 rounded'>
-                        <p className='cursor-pointer hover:text-black'>Profile Info</p>
-                        <p onClick={() => navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
-                        <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
-                    </div>
-                </div> }
-             </div>
+             <div className="group relative">
+                <img src={assets.profile_icon} className="w-5 cursor-pointer" alt="profile"
+                onClick={() => {
+                    if (!token) {navigate("/login");
+                    } else {
+                        setOpen(!open);
+                    }
+                }}/>
+
+        {/* Dropdown Menu */}
+        {token && open && (
+            <div className="group-hover:block absolute right-0 mt-4 z-50">
+                <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-gray-200 text-gray-600 rounded shadow-md">
+                <p onClick={() => {navigate("/"); setOpen(false);}} 
+                className="cursor-pointer hover:text-black">Profile Info</p>
+                <p onClick={() => {navigate("/orders"); setOpen(false);}}
+                className="cursor-pointer hover:text-black">Orders</p>
+                <p onClick={() => {logout(); setOpen(false);}}className="cursor-pointer hover:text-black">Logout</p>
+                </div>
+            </div>
+        )}
+    </div>
+
              <Link to='/cart' className='relative'>
              <img src={assets.cart_icon} className='w-5 min-w-5' alt=''/>
              <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[-8px]'>{getCartCount()}</p>
